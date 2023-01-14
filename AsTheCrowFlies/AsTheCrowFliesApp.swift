bdbdfec53b7c9c386f0a3ba19c64eddd7c7bf8ec
Joshua Kaden <jkaden@mac.com>
2023-01-14 13:23:41 -0500

    Initial Commit

---
+++ AsTheCrowFlies/AsTheCrowFliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsTheCrowFliesApp.swift
+//  AsTheCrowFlies
+//
+//  Created by Joshua Kaden on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AsTheCrowFliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
